7a03d46e0cbc0aa9c7df252d73187d1a0bc106e6
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-24 23:01:07 -0300

    first commit

---
+++ lib/models/language.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Language {
+  final String title;
+  final String subtitle;
+  bool select = false;
+  IconData icon = Icons.add_reaction;
+
+  Language(this.title, this.subtitle);
+}
